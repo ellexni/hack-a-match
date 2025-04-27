314c1af7b4ef6bc704173f66f36b2b302cb52fc4
Denielle Camacho <camachodenielle@gmail.com>
2025-04-27 17:18:15 -0400

    created the match cards

---
+++ src/components/Match.jsx
@@ -0,0 +1,27 @@
+function Match({ profile }) {
+    return (
+        <div className="match-card">
+            <h2 className="match-name">{profile.full_name}</h2>
+
+            <label className="match-label">Education</label>
+            <p className="match-info">{profile.education || "No education added"}</p>
+
+            <label className="match-label">Skills</label>
+            <p className="match-info">
+                {Array.isArray(profile.skills) && profile.skills.length > 0 ? profile.skills.join(", ") : "No skills added"}
+            </p>
+
+            <label className="match-label">Preferred Roles</label>
+            <p className="match-info">
+                {Array.isArray(profile.roles) && profile.roles.length > 0 ? profile.roles.join(", ") : "No preferred roles added"}
+            </p>
+
+            <label className="match-label">Participating in</label>
+            <p className="match-info">
+                {Array.isArray(profile.hackathons) && profile.hackathons.length > 0 ? profile.hackathons.join(", ") : "No hackathons added"}
+            </p>
+        </div>
+    );
+}
+
+export default Match;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,22 @@
-import Navbar from "../components/Navbar"
+import Navbar from "../components/Navbar";
+import Match from "../components/Match";
+import { useState, useEffect } from "react";
+import { supabase } from "../client";
 
 function Homepage({token}) {
+
+  const [profiles, setProfiles] = useState([]);
+
+  async function fetchProfiles() {
+    const { data, error } = await supabase.from("profiles").select("*");
+
+    if (error) console.error(error);
+    else setProfiles(data);
+  }
+
+  useEffect(() => {
+    fetchProfiles();
+  }, []);
 
   return (
     
@@ -9,6 +25,16 @@ function Homepage({token}) {
         <Navbar />
 
         <h3>Welcome back, {token.user.user_metadata.full_name}</h3>
+
+        <h3>Your Matches</h3>
+        <div className="matches">
+        {profiles
+          .filter(profile => profile.id !== token.user.id)
+          .map(profile => (
+            <Match key={profile.id} profile={profile}/>
+          ))
+        }
+      </div> 
 
     </div>
   )
